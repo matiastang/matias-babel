01346eb7f326d5631f9c8ca9958b16b24e18d50e
唐道勇 <matias@tangdaoongdeMBP.lan>
2021-10-12 17:51:51 +0800

    feat: - 添加removeConsole插件根据类型移除

--- src/removeConsole.js
+++ src/removeConsole.js
@@ -1,35 +1,123 @@
 /*
  * @Author: matiastang
  * @Date: 2021-10-12 15:14:26
- * @LastEditTime: 2021-10-12 16:27:14
+ * @LastEditTime: 2021-10-12 17:49:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /matias-babel/src/removeConsole.js
  */
-const babel = require('@babel/core')
 const get = require('lodash/get')
 const eq = require('lodash/eq')
 
-const { transformAsync } = babel
+/**
+ * 类型
+ */
+const _CONSOLE_ENUM = {
+    /**
+     * time
+     * timeEnd
+     */
+    TIME: 'time',
+    /**
+     * group
+     * groupCollapsed
+     * groupEnd
+     */
+    GROUP: 'group',
+    /**
+     * assert
+     */
+    ASSERT: 'assert',
+    /**
+     * count
+     */
+    COUNT: 'count',
+    /**
+     * error
+     */
+    ERROR: 'error',
+    /**
+     * info
+     */
+    INFO: 'info',
+    /**
+     * log
+     */
+    LOG: 'log',
+    /**
+     * table
+     */
+    TABLE: 'table',
+    /**
+     * clear
+     */
+    CLEAR: 'clear',
+    /**
+     * warn
+     */
+    WARN: 'warn',
+    /**
+     * trace
+     */
+    TRACE: 'trace',
+}
+
+/**
+ * 获取需要移除的Console类型
+ * @param {*} propertys 
+ * @returns 
+ */
+const _getRemoveConsolePropertys = (propertys = []) => {
+    let arr = []
+    for (let value of Object.values(_CONSOLE_ENUM)) {
+        if (propertys.includes(value)) {
+            if (value === _CONSOLE_ENUM.TIME) {
+                arr.push('time')
+                arr.push('timeEnd')
+            } else if (value === _CONSOLE_ENUM.GROUP) {
+                arr.push('group')
+                arr.push('groupCollapsed')
+                arr.push('groupEnd')
+            } else {
+                arr.push(value)
+            }
+        }
+    }
+    return arr
+}
 
 /**
  * 异常console输出
  * **注意**如果=>不使用()则不是返回的对象，将报错：throw new Error("Plugin/Preset did not return an object.");
  * @param {*} rootPath 
  */
-const removeConsole = rootPath => ({
-    name: "ast-transform-remove-console",
-    visitor: {
-        ExpressionStatement: path => {
-            // 获取console
-            const name = get(path, 'node.expression.callee.object.name')
-            const CONSOLE = 'console'
-            // DOTO: - 可以更精准的定位，比如只移除console.log()的输入
-            if (eq(name, CONSOLE)) {
-                path.remove()
+const removeConsole = (propertys = []) => {
+    let propertyArr = []
+    if (propertys.length > 0) {
+        propertyArr = _getRemoveConsolePropertys(propertys)
+    }
+    const CONSOLE = 'console'
+    return rootPath => ({
+        name: "ast-transform-remove-console",
+        visitor: {
+            ExpressionStatement: path => {
+                // 获取console
+                const object_name = get(path, 'node.expression.callee.object.name')
+                // 匹配console
+                if (eq(object_name, CONSOLE)) {
+                    if (propertyArr.length > 0) {
+                        const property_name = get(path, 'node.expression.callee.property.name')
+                        // 匹配如: log
+                        if (propertyArr.includes(property_name)) {
+                            path.remove()
+                        }
+                    } else {
+                        path.remove()
+                    }    
+                }
             }
         }
-    }
-})
+    })
+} 
 
 module.exports = removeConsole
