a92a2d3191782c1563d52841f0c7ae6fd1b621a6
唐道勇 <matias@tangdaoongdeMBP.lan>
2021-10-12 17:52:44 +0800

    feat: - 添加removeConsole插件根据类型移除

--- test/removeConsole.test.js
+++ test/removeConsole.test.js
@@ -1,41 +1,11 @@
-<!--
+/*
  * @Author: your name
- * @Date: 2021-10-12 15:08:38
- * @LastEditTime: 2021-10-12 17:51:00
+ * @Date: 2021-10-12 15:31:53
+ * @LastEditTime: 2021-10-12 17:50:30
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: /matias-babel/README.md
--->
-# matias-babel
-
-Babel 学习、使用、练习
-
-## 允许
-
-使用`pnpm`管理依赖。
-
-* 下载依赖：
-```
-pnpm add
-```
-
-* 运行测试
-```
-pnpm run test
-```
-或
-```
-pnpx run test
-```
-
-## babel插件
-
-### removeConsole
-
-去除代码中的所有打印输出。
-
-示例：
-```js
+ * @FilePath: /matias-babel/test/removeConsole.test.js
+ */
 const babel = require('@babel/core')
 const removeConsole = require('../src/removeConsole')
 
@@ -90,11 +60,8 @@ test('babel插件`remove console` 移除log测试', async () => {
         ${value}
         console.log(a);
     `
+    console.log(oldCode)
     let newCode = await transformCode(oldCode, ['log']);
+    console.log(newCode)
     expect(newCode).toEqual(value)
 })
-```
-
-## 测试
-
-使用[`Jest`](https://www.jestjs.cn/)做测试，
